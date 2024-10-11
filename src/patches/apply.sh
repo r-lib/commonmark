3c4600eaf6a55bbd35837d6c38d49699f9b777fd
Yihui Xie <xie@yihui.name>
2024-10-11 10:32:32 -0500

    git diff be62b2b26a06e0143144a444740f86491e4413ce c3be50ec6d2cbef983d68dbab16cde8c91a1c7a8 > src/patches/latex-footnotes.diff

--- src/patches/apply.sh
+++ src/patches/apply.sh
@@ -2,4 +2,5 @@
 
 # Upstream PR: https://github.com/github/cmark-gfm/pull/362
 patch -p2 -d ../cmark < 362.diff
-
+# Support footnotes for LaTeX: https://github.com/r-lib/commonmark/pull/32
+patch -p2 -d ../cmark < latex-footnotes.diff
